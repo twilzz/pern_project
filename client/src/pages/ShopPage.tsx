999e90fa0bd085ce9e0e4d68428e23d940c8d65a
Alexey Sladkov <twils@ya.ru>
2024-03-29 21:09:25 +0300

    add data for mapping add card component from shadcn lib

--- client/src/pages/ShopPage.tsx
+++ client/src/pages/ShopPage.tsx
@@ -1,3 +1,83 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { ReactNode } from 'react'
+
+const products = [
+  {
+    id: 1,
+    title: 'title 1',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, perferendis?',
+    rating: 4,
+  },
+  {
+    id: 1,
+    title: 'title 1',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, perferendis?',
+    rating: 4,
+  },
+  {
+    id: 1,
+    title: 'title 1',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, perferendis?',
+    rating: 4,
+  },
+  {
+    id: 1,
+    title: 'title 1',
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, perferendis?',
+    rating: 3,
+  },
+]
+
 export const ShopPage = () => {
-    return <div>Shop Page!</div>
+  return (
+    <div>
+      <h1>Shop Page!</h1>
+      <div>
+        {products.map(({ id, title, description, rating }) => (
+          <ShopCard
+            key={id}
+            title={title}
+            description={description}
+            content={description}
+            footer={rating}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+interface ShopCardProps {
+  title: string
+  description: string
+  content: string
+  footer: ReactNode | string
+}
+
+const ShopCard = ({ title, description, content, footer }: ShopCardProps) => {
+  return (
+    <Card className="w-[300px]">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{content}</p>
+      </CardContent>
+      <CardFooter>
+        <p>{footer}</p>
+      </CardFooter>
+    </Card>
+  )
 }
